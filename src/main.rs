5992a2e511c223cf789348ad43d648eb8ff73285
rap2hpoutre <raphaelht@gmail.com>
2016-07-01 17:45:16 +0200

    init

--- src/main.rs
+++ src/main.rs
@@ -4,16 +4,18 @@ use hyper::client::Client;
 use std::io::Read;
 
 fn main () {
+    let reddit_content = get_url_content("https://www.reddit.com/r/programming/.rss");
 
+    println!("Result: {}", reddit_content);    
+}
+
+fn get_url_content(url: &str) -> String {
     let client = Client::new();
     let mut s = String::new();
-
-    client.get("https://www.reddit.com/r/programming/.rss")
+    client.get(url)
         .send()
         .unwrap()
         .read_to_string(&mut s)
         .unwrap();
-
-    println!("Result: {}", s);
-    
+    s
 }
